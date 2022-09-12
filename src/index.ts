2404ccea8af8cf0298e7ebdf66255407b7eed8b0
Natan Pereira <ntnp@ntnp.dev>
2022-09-12 16:34:51 -0300

    feat!: null-safe `get`

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,22 @@ import { Path, PathValue } from './types';
 
 export * from './types';
 
-export function get<O extends object, P extends Path<O>> (
+export function getOrFail<O extends object, P extends Path<O>> (
     source: O,
     path: P & string,
     options?: IPathOptions,
 ): PathValue<O, P> {
     return resolveValue(source, path, options);
+}
+
+export function get<O extends object, P extends Path<O>> (
+    source: O,
+    path: P & string,
+    options?: IPathOptions,
+): PathValue<O, P> | null {
+    try {
+        return getOrFail(source, path, options);
+    } catch (err) {
+        return null;
+    }
 }
